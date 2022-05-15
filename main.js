b3ee33b6452d598c2c62cb44f1daefc8f7169058
SWASTIK SIBAM <89509745+GITHUBSHJ@users.noreply.github.com>
2022-05-15 23:30:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log(
+	"%cMADE BY - SWASTIK SIBAM NAYAK",
+	"color: white; background:linear-gradient(#E66465, #9198E5); padding: 1.2em; border-radius: 6px;"
+ );
+
+ console.log("i will do coding i next class........");
+ console.log("nothing to say");
+ console.log("BYE!!!!")
+
+//**I WILL DO coding IN NEXT CLASS PROJECT*/ 
